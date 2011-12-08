7447c2aa67b46d8403e304b3d76d5fa302c07559
andrew_nelson <andrew_nelson@0ab8e41a-71a8-43ab-b14f-4054c5750dd2>
2011-12-08 05:09:55 +0000

    fixed up SLIM installer

--- branches/dev/installers/postflight/motofitpostflight.sh
+++ branches/dev/installers/postflight/motofitpostflight.sh
@@ -15,8 +15,11 @@ then
 fi
 
 
-if [ -d /Applications/Igor\ Pro\ Folder/Igor\ Extensions/HDF5.xop\ alias ]
+if [ -d /Applications/Igor\ Pro\ Folder/ ]
 then
-	rm /Applications/Igor\ Pro\ Folder/Igor\ Extensions/HDF5.xop\ alias
-fi
 ln -sf /Applications/Igor\ Pro\ Folder/More\ Extensions/File\ Loaders/HDF5.xop ~/Documents/Wavemetrics/Igor\ Pro\ 6\ User\ Files/Igor\ Extensions/HDF5.xop\ alias
+fi
+if [ -d /Applications/Igor\ Pro\ 6.2\ Folder/ ]
+then
+ln -sf /Applications/Igor\ Pro\ 6.2\ Folder/More\ Extensions/File\ Loaders/HDF5.xop ~/Documents/Wavemetrics/Igor\ Pro\ 6\ User\ Files/Igor\ Extensions/HDF5.xop\ alias
+fi
